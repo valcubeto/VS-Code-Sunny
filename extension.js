2107cacdc916fe06b71c991fb4d647eedd77c8ae
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2022-03-22 19:06:06 -0300

    completion items (1)

--- extension.js
+++ extension.js
@@ -1,47 +1,14 @@
 const vscode = require('vscode');
 
-/** @param {vscode.ExtensionContext} context */
-function activate(context) {
+/** @param {Array} {subscriptions} */
+function activate({ subscriptions }) {
 	vscode.window.showInformationMessage('Quara extension activated');
+
+	const provider = vscode.languages.registerCompletionItemProvider('quara', {
+		provideCompletionItems: () => require('./completion-items.json')
+	}, '.');
+
+	subscriptions.push(provider);
 }	
 	
 module.exports = { activate };
-
-
-/*
-	context.subscriptions.push(
-		vscode.languages.registerCompletionItemProvider(
-			'quara',
-			new vscode.CompletionItemProvider().provideCompletionItems(
-				new vscode.TextDocument(),
-				new vscode.Position(),
-				new vscode.CancellationToken(),
-				new vscode.CompletionContext()
-			),
-			'.'
-		)
-	);
-*/
-
-// when intentas hacer que funcione el autocompletado y el hover funcionen pero no funcionan ni los de javascript XDdXDXXDx
-
-/*
-	context.subscriptions.push(
-		vscode.languages.registerCompletionItemProvider(0, new class extends  {
-			provideCompletionItems(document, position, tokens) {
-				vscode.window.showInformationMessage(`Completion at ${position.line + 1}:${position.character + 1}`);
-			}
-		}, '.')
-	);
-	vscode.languages.registerHoverProvider('quara', {
-		provideHover(document, position, token) {
-			return {
-				contents: [`Hover at ${position.line + 1}:${position.character + 1}`]
-			};
-		}
-	});
-*/
-
-vscode.workspace.createFileSystemWatcher
-
-// https://thatpervert.com/tag/kamuo
