a07337ee7af6360cf9d0095411cbeeb3b0479c0f
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2022-03-22 20:45:19 -0300

    first completion item

--- extension.js
+++ extension.js
@@ -1,14 +1,12 @@
 const vscode = require('vscode');
+const items = require('./completion-items.json');
+items.forEach(item => {
+	item.documentation = new vscode.MarkdownString('```quara\n$\n```'.replace('$', item.documentation));
+});
 
-/** @param {Array} {subscriptions} */
 function activate({ subscriptions }) {
-	vscode.window.showInformationMessage('Quara extension activated');
-
-	const provider = vscode.languages.registerCompletionItemProvider('quara', {
-		provideCompletionItems: () => require('./completion-items.json')
-	}, '.');
-
+	const provider = vscode.languages.registerCompletionItemProvider('quara', { provideCompletionItems: () => items }, '.');
 	subscriptions.push(provider);
-}	
-	
+}
+
 module.exports = { activate };
