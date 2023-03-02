b0458c66590f5c3a7f211ee0d450c5ee9d90f354
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-03-02 17:16:58 -0300

    .

--- extension.js
+++ extension.js
@@ -1,103 +1,142 @@
 const vscode = require('vscode')
 
 try {
-const items = require('./completion-items.json')
+	const items = require('./completion-items.json')
 
-/** 
- * @typedef {{  }} CompletionItem
- * @typedef {typeof vscode.CompletionItem} VSCCompletionItem
- */
+	/** 
+	 * @typedef {{  }} CompletionItem
+	 * @typedef {typeof vscode.CompletionItem} VSCCompletionItem
+	 */
 
-/** @type {CompletionItem} */
-const completionItems = []
+	/** @type {CompletionItem} */
+	const completionItems = []
 
-/**
- * If the value is an array returns it joined by a new line
- * @param {string | string[]} value The string or array
- * @returns {string} The string or the joined array
- */
-function joinIfArray(value) {
-	if (Array.isArray(value))
-		return value.join('\n')
-	else
-		return `${value}`
-}
-
-const KindMap = {
-	'keywords': 'Keyword',
-	'types': 'Struct',
-	'functions': 'Function'
-}
-
-/**
- * @param {CompletionItem} item
- * @param {string} kind
- * @returns {VSCCompletionItem}
- */
-function createCompletionItem(item, kind) {
-	const documentation = []
-
-	if (item.description) {
-		documentation.push(item.description, '')
+	/**
+	 * If the value is an array returns it joined by a new line
+	 * @param {string | string[]} value The string or array
+	 * @returns {string} The string or the joined array
+	 */
+	function joinIfArray(value) {
+		if (Array.isArray(value))
+			return value.join('\n')
+		else
+			return `${value}`
 	}
 
-	if (item.documentation) {
-		documentation.push(
-			'**Documentation**',
-			'```quantum',
-			joinIfArray(item.documentation),
-			'```'
+	const KindMap = {
+		'keywords': 'Keyword',
+		'types': 'Struct',
+		'functions': 'Function'
+	}
+
+	/**
+	 * @param {CompletionItem} item
+	 * @param {string} kind
+	 * @returns {VSCCompletionItem}
+	 */
+	function createCompletionItem(item, kind) {
+		const documentation = []
+
+		if (item.description) {
+			documentation.push(item.description, '')
+		}
+
+		if (item.documentation) {
+			documentation.push(
+				'**Documentation**',
+				'```quantum',
+				joinIfArray(item.documentation),
+				'```'
+			)
+		}
+
+		if (item.example) {
+			documentation.push(
+				'**Example**',
+				'```quantum',
+				joinIfArray(item.example),
+				'```'
+			)
+		}
+
+		const completionItem = {
+			kind: vscode.CompletionItemKind[KindMap[kind]],
+			label: item.name,
+			detail: `${kind.slice(0, -1)}: ${item.name}`,
+			documentation: new vscode.MarkdownString(documentation.join('\n'))
+		}
+
+		return completionItem
+	}
+
+	for (const kind in items) {
+		items[kind].forEach(item => {
+			const completionItem = createCompletionItem(item, kind)
+			completionItems.push(completionItem)
+		})
+	}
+
+	function activate({ subscriptions }) {
+		const completionItemProvider = vscode.languages.registerCompletionItemProvider('quantum', {
+			provideCompletionItems(document, position, token, context) {
+				return completionItems
+			}
+		})
+		const hoverProvider = vscode.languages.registerHoverProvider('quantum', {
+			provideHover(document, position, token, next) {
+				if (document.isUntitled) return 
+				// const sunny = cp.spawn('bin/sunny-analizer', [`--line=${position.line}`, `--char=${position.character}`, document.uri.fsPath])
+				/*
+				*/
+				// vscode.window.showInformationMessage(JSON.stringify({ document, position, token, next }, null, 2))
+				/*
+				{
+					"document": {
+						"uri": {
+							"$mid": 1,
+							"fsPath": "c:\\Users\\valen\\OneDrive\\Escritorio\\finally-qmark\\docs.qua",
+							"_sep": 1,
+							"external": "file:///c%3A/Users/valen/OneDrive/Escritorio/finally-qmark/docs.qua",
+							"path": "/C:/Users/valen/OneDrive/Escritorio/finally-qmark/docs.qua",
+							"scheme": "file"
+						},
+						"fileName": "c:\\Users\\valen\\OneDrive\\Escritorio\\finally-qmark\\docs.qua",
+						"isUntitled": false,
+						"languageId": "quantum",
+						"version": 1,
+						"isClosed": false,
+						"isDirty": false,
+						"eol": 1,
+						"lineCount": 103
+					},
+					"position": {
+						"line": 72,
+						"character": 62
+					},
+					"token": {
+						"a": false,
+						"b": null
+					}
+				}
+				*/
+				return [
+					{
+						name: 'void',
+						documentation: 'type void'
+					}
+				]
+			}
+		})
+
+		subscriptions.push(
+			completionItemProvider,
+			hoverProvider
 		)
 	}
 
-	if (item.example) {
-		documentation.push(
-			'**Example**',
-			'```quantum',
-			joinIfArray(item.example),
-			'```'
-		)
-	}
+	module.exports = { activate }
 
-	const completionItem = {
-		kind: vscode.CompletionItemKind[KindMap[kind]],
-		label: item.name,
-		detail: `${kind.slice(0, -1)}: ${item.name}`,
-		documentation: new vscode.MarkdownString(documentation.join('\n'))
-	}
-
-	return completionItem
-}
-
-for (const kind in items) {
-	items[kind].forEach(item => {
-		const completionItem = createCompletionItem(item, kind)
-		completionItems.push(completionItem)
-	})
-}
-
-function activate({ subscriptions }) {
-	const completionItemProvider = vscode.languages.registerCompletionItemProvider('quantum', {
-		provideCompletionItems(document, position, token, context) {
-			return completionItems
-		}
-	})
-	const hoverProvider = vscode.languages.registerHoverProvider('quantum', {
-		provideHover(document, position, token, next) {
-			vscode.window.showInformationMessage(JSON.stringify({ document, position, token, next }))
-			return []
-		}
-	})
-
-	subscriptions.push(
-		completionItemProvider,
-		hoverProvider
-	)
-}
-
-module.exports = { activate }
-
-// export type ProviderResult<T> = T | undefined | null | Thenable<T | undefined | null>;
+	// export type ProviderResult<T> = T | undefined | null | Thenable<T | undefined | null>;
 } catch (error) {
 	vscode.window.showErrorMessage(error.toString())
 }
